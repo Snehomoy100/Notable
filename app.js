d44b060c23bbcbe47fe5c9f5981f1a772612ef13
snehomoy100 <snehomoy100@gmail.com>
2021-04-19 19:08:36 +0530

    app.js updated

--- app.js
+++ app.js
@@ -1,10 +1,12 @@
+const chalk = require('chalk');
+
 const validator = require('validator');
 
 // const add = require('./utils');
+
 const getNotes = require('./notes');
 
 // const sum = add(3, 86);
-
 
 const msg = getNotes();
 
@@ -13,4 +15,10 @@ console.log(msg);
 console.log(validator.isEmail('name@example.com'));
 
 console.log(validator.isURL('https:/ok.com'));
+
+const greenMsg = chalk.red.bold.italic.inverse('Error!');
+
+console.log(greenMsg);
+
+
 
