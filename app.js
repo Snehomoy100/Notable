32ee6b09787838253a0643e35f9510da2698b302
snehomoy100 <snehomoy100@gmail.com>
2021-04-23 22:32:38 +0530

    app.js file updated

--- app.js
+++ app.js
@@ -4,8 +4,66 @@ const yargs = require('yargs');
 
 // Command line arguement using process.argv
 
+// Create add functionality
 
-console.log(process.argv);
+// add, remove, read & list all the notes.
+
+yargs.command({
+    command: 'add',
+    describe: 'Add a note',
+    builder: {
+      title: {
+          describe: 'Note Title',
+          demandOption: true,
+          type: 'string'
+      },
+      body: {
+          describe: 'Note Body',
+          demandOption: true,
+          type: 'string'
+      }  
+    },
+    handler: function (args) {
+        console.log('Title = ' + args.title);
+        console.log('Body = ' + args.body);
+    }
+});
+
+// Create remove functionality
+
+yargs.command({
+    command: 'remove',
+    describe: 'Remove a note',
+    handler: function () {
+        console.log('Removing a note!')
+    }
+});
+
+// Create read notes
+
+yargs.command({
+    command: 'read',
+    describe: 'Read the note',
+    handler: function(){
+        console.log('Reading the note!');
+    }
+});
+
+// create list of the notes
+
+yargs.command({
+    command: 'list',
+    describe: 'List of the notes',
+    handler: function(){
+        console.log('Listing down all the notes!');
+    }
+});
+
+
+yargs.parse();
+
+
+// console.log(yargs.argv);
 
 
 
