61aa3108a75c19df9ad918aa694a04ba261fd5d6
snehomoy100 <snehomoy100@gmail.com>
2021-04-27 13:42:53 +0530

    add & remove note functionaity added

--- Arrow-func.js
+++ Arrow-func.js
@@ -8,8 +8,23 @@
 // };
 
 
-const square = (x) => x * x;
+// const square = (x) => x * x;
+// console.log(square(7));
 
 
-console.log(square(7));
+const myEvent = {
+    name: 'Birthday Party',
+    guestList: ['Snehomoy', 'Rupomoy'],
+    printGuestlist() {
+        console.log("Guests are coming in my " + this.name + '.');
+
+        this.guestList.forEach((guest) => {
+            console.log(guest + " is attending the " + this.name);
+        })
+    }
+
+
+};
+
+myEvent.printGuestlist();
 
